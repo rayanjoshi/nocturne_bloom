685270e23c6bec8ca9068983658a69c01ad48502
Rayan Joshi <rj524@ic.ac.uk>
2025-07-20 17:09:03 +0100

    refactor: revise __init__.py imports to accommodate new files

--- NVDA_stock_predictor/src/__init__.py
+++ NVDA_stock_predictor/src/__init__.py
@@ -1,8 +1,9 @@
 from backtest import *
 from data_loader import load_data
 from feature_engineering import feature_engineering
+from data_module import data_module
 from model_baseline import *
-from model_CNN import *
+from model_CNNLSTM import *
 from utils import *
 
-__all__ = [ "load_data", "feature_engineering"]
+__all__ = [ "load_data", "feature_engineering", "data_module", "model_CNNLSTM"]
